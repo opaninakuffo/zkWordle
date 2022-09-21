219cad0258166bbb800861fdb8aecb3fe022fa26
opaninakuffo <opaninakuffo@outlook.com>
2022-09-21 19:31:43 +0100

    correct url

--- app.js
+++ app.js
@@ -27,4 +27,4 @@ app.use((err, req, res, next) => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 @ http://127.0.0.1:/${PORT}`));
+app.listen(PORT, () => console.log(`🚀 @ http://127.0.0.1:${PORT}`));
